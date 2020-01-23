40c23b383618c00694f5d86b71547a6303d197ad
Aaron Clare <aaronclare88@gmail.com>
2020-01-22 20:48:28 -0800

    php9

--- form.php
+++ form.php
@@ -1,39 +1,54 @@
 <?php
+ include_once "vendor/autoload.php";
 
- $url = 'https://api.sendgrid.com/';
- $user = 'USERNAME';
- $pass = 'PASSWORD';
+ $text = "Hi!\nHow are you?\n";
+ $html = "<html>
+       <head></head>
+       <body>
+           <p>Hi!<br>
+               How are you?<br>
+           </p>
+       </body>
+       </html>";
+ // This is your From email address
+ $from = array('j.overby@comcast.net' => 'Name To Appear');
+ // Email recipients
+ $to = array(
+       'aaronclare88@gmail.com'=>'Destination 1 Name',
+ );
+ // Email subject
+ $subject = 'Example PHP Email';
 
- $params = array(
-      'api_user' => $user,
-      'api_key' => $pass,
-      'to' => 'aaronclare88@gmail.com',
-      'subject' => 'testing from curl',
-      'html' => 'testing body',
-      'text' => 'testing body',
-      'from' => 'some name here',
-   );
+ // Login credentials
+ $username = 'Aaron Clare';
+ $password = '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';
 
- $request = $url.'api/mail.send.json';
+ // Setup Swift mailer parameters
+ $transport = Swift_SmtpTransport::newInstance('smtp.sendgrid.net', 587);
+ $transport->setUsername($username);
+ $transport->setPassword($password);
+ $swift = Swift_Mailer::newInstance($transport);
 
- // Generate curl request
- $session = curl_init($request);
+ // Create a message (subject)
+ $message = new Swift_Message($subject);
 
- // Tell curl to use HTTP POST
- curl_setopt ($session, CURLOPT_POST, true);
+ // attach the body of the email
+ $message->setFrom($from);
+ $message->setBody($html, 'text/html');
+ $message->setTo($to);
+ $message->addPart($text, 'text/plain');
 
- // Tell curl that this is the body of the POST
- curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+ // send message
+ if ($recipients = $swift->send($message, $failures))
+ {
+     // This will let us know how many users received this message
+     echo 'Message sent out to '.$recipients.' users';
+ }
+ // something went wrong =(
+ else
+ {
+     echo "Something went wrong - ";
+     print_r($failures);
+ }
 
- // Tell curl not to return headers, but do return the response
- curl_setopt($session, CURLOPT_HEADER, false);
- curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-
- // obtain response
- $response = curl_exec($session);
- curl_close($session);
-
- // print everything out
- print_r($response);
-
- ?>
+?>
