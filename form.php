4fb45456318baa6f23e699d400ec0c2f980825de
Aaron Clare <aaronclare88@gmail.com>
2020-01-22 20:13:01 -0800

    php6

--- form.php
+++ form.php
@@ -7,11 +7,17 @@
     $subject = 'Customer Inquiry';
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
-    if ($_POST['submit']) {
-        if (mail ($to, $subject, $body, $from)) {
-            echo '<p>Your message has been sent!</p>';
-        } else {
-            echo '<p>Something went wrong, go back and try again!</p>';
-        }
+    $header = "From: noreply@example.com\r\n";
+    $header.= "MIME-Version: 1.0\r\n";
+    $header.= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    $header.= "X-Priority: 1\r\n";
+
+    $status = mail($to, $subject, $message, $header);
+
+    if($status)
+    {
+        echo '<p>Your mail has been sent!</p>';
+    } else {
+        echo '<p>Something went wrong. Please try again!</p>';
     }
 ?>
